d4fec15ede2df65274a4687a3aa29fdc0252d32d
GiuPassarelli <gb_passarelli@hotmail.com>
2018-10-26 11:22:46 -0300

    AM done

--- AM.py
+++ AM.py
@@ -13,23 +13,9 @@ import scipy.signal as signal
 
 sinal = signalMeu()
 
-def generateTecla(freq,amp,time): #TODO: deixar a funcao mais versatil com valores padroes & colocar samples como variavel (fs)
-	resp = np.add(sinal.generateSin(freq[0],amp,time,48000), sinal.generateSin(freq[1],amp,time,48000))
-	return resp
-
-
 def generateAm(freq,rec,fs=48000,t=2): #TODO: deixar a funcao mais versatil com valores padroes & colocar samples como variavel (fs)
 	resp = np.multiply(sinal.generateSin(freq,1,t,fs)[1], rec)
 	return resp
-
-def getTopFreq(freq,maxdiff):
-	resp = [freq[0]] #TODO: melhorar funcao para nao assumir o primeiro valor e sim o maior valor entre eles, TIP: colocar uma lista temporaria
-	for i in freq:
-		if maxdiff < np.abs(resp[-1] - i):
-			resp.append(i)
-	return resp
-
-
 
 def FIRFilter(yAudioNormalizado, samplerate = 48000, dbb = 60.0, cutoff_hz = 4000.0):
 	nyq_rate = samplerate/2
@@ -40,51 +26,63 @@ def FIRFilter(yAudioNormalizado, samplerate = 48000, dbb = 60.0, cutoff_hz = 400
 	yFiltrado = signal.lfilter(taps, 1.0, yAudioNormalizado)
 	return yFiltrado
 
+def plotGraphs(s1, s2):
+	plt.subplot(2, 1, 1)
+	plt.plot(np.arange(0,1,1/48000)[:500],s1[:500])
+	plt.ylabel('Original')
+
+	plt.subplot(2, 1, 2)
+	plt.plot(np.arange(0,1,1/48000)[:500],s2[:500])
+	plt.ylabel('Normalizado')
+
+	plt.show()
 
 tecla = input("Tecla: ")
+
 if tecla[0] == "s":
 	myrecording = sd.rec(int(int(tecla[1]) * 48000), samplerate=48000, channels=1) #TODO: fazer uma funcao para esta linha
 	sd.wait()
+
 	myrecording = np.ndarray.flatten(myrecording)
+	sinalNormalizado = myrecording/np.max(myrecording)
+	sinalFiltrado = FIRFilter(sinalNormalizado,48000,60.0,4000.0)
+	AM=generateAm(12000,sinalFiltrado,48000,int(tecla[1]))
 
-	myrecording /= np.max(myrecording)
-
-
-	AM=generateAm(12000,myrecording,48000,int(tecla[1]))
+	plotGraphs(myrecording,sinalNormalizado)
+	plotGraphs(sinalFiltrado,AM)
 
 	sd.play(AM,48000)
 	sd.wait()
 
 elif tecla=="fs":
-	data, fs = sf.read("senda.ogg", dtype='float32')
+	myrecording, fs = sf.read("senda.ogg", dtype='float32')
 	fs=48000
-	data /= np.max(data) #normalizando o vetor do audio do arquivo
-	time = len(data)/fs
-	AM=generateAm(12000,data,fs,time)
-	AMClean=generateAm(12000,AM,fs,time)
-	final = FIRFilter(AMClean,fs,60,12000)
-	print(final)
 
+	sinalNormalizado = myrecording/np.max(myrecording) #normalizando o vetor do audio do arquivo
+	time = len(sinalNormalizado)/fs
+	sinalFiltrado = FIRFilter(sinalNormalizado,fs,60.0,4000.0)
+	AM=generateAm(12000,sinalFiltrado,fs,time)
+
+	plotGraphs(myrecording,sinalNormalizado)
+	plotGraphs(sinalFiltrado,AM)
+
+	print(AM)
 
 	sd.play(AM,fs)
 	sd.wait()
 
 elif tecla[0] == "r":
-	pass
 
-		#freqsort = [x for _, x in sorted(zip(calcu,freq))] # ultimo item é o maior
-
-
-		#print("CALCU: ", calcu)
-		#print(getTopFreq(freqsort,2)[-5:-1])
-
-
-else:
-	sd.play(generateTecla(dictTecla[tecla],1,1)[1],48000)
+	fs=48000
+	myrecording = sd.rec(int(10*fs), samplerate=fs, channels=1) #TODO: fazer uma funcao para esta linha 
 	sd.wait()
-	print(generateTecla(dictTecla[tecla],1,1)[1],48000)
-	#sinal.plotFFT(generateTecla(dictTecla[tecla],1,1),48000)
-	plt.plot(generateTecla(dictTecla[tecla],1,1)[0][:500],generateTecla(dictTecla[tecla],1,1)[1][:500])
-	plt.draw()
-	plt.pause(pltDelay)
-	plt.close()
+	myrecording = np.ndarray.flatten(myrecording)
+	time = len(myrecording)/fs
+
+	AM = generateAm(12000,myrecording,48000,time)
+	final = FIRFilter(AM,fs,60.0,4000.0)
+
+	sd.play(final,fs)
+	sd.wait()
+
+	plotGraphs(myrecording,final)
