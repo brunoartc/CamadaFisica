295a75d1557a818418b085ce78f50abe34f9801c
brunoartc <brunoartc@yahoo.com>
2018-11-01 16:27:45 -0300

    organize repository

--- AM/AM.py
+++ AM/AM.py
@@ -26,16 +26,29 @@ def FIRFilter(yAudioNormalizado, samplerate = 48000, dbb = 60.0, cutoff_hz = 400
 	yFiltrado = signal.lfilter(taps, 1.0, yAudioNormalizado)
 	return yFiltrado
 
-def plotGraphs(s1, s2):
+def plotGraphs(s1, s2,fs=48000, time=1):
+	print("fs=",fs,"time=",time,"1/fs=",1/fs)
+	print(s1)
 	plt.subplot(2, 1, 1)
-	plt.plot(np.arange(0,1,1/48000)[:500],s1[:500])
-	plt.ylabel('Original')
+	plt.plot(np.arange(0,time,1/fs)[:5000],s1[:5000])
+	plt.ylabel(np.max(s1))
 
 	plt.subplot(2, 1, 2)
-	plt.plot(np.arange(0,1,1/48000)[:500],s2[:500])
-	plt.ylabel('Normalizado')
+	plt.plot(np.arange(0,time,1/fs)[:5000],s2[:5000])
+	plt.ylabel(np.max(s2))
 
 	plt.show()
+def tryPlot(s1, fs=48000, time=1):
+
+
+
+	plt.plot(range(len(s1))[:int(len(s1)/3)],s1[:int(len(s1)/3)])
+
+
+
+	plt.title(np.max(s1))
+	plt.show()
+
 
 tecla = input("Tecla: ")
 
@@ -44,9 +57,18 @@ if tecla[0] == "s":
 	sd.wait()
 
 	myrecording = np.ndarray.flatten(myrecording)
-	sinalNormalizado = myrecording/np.max(myrecording)
+	sinalNormalizado = ((myrecording-np.min(myrecording))/(np.max(myrecording)-np.min(myrecording)))
 	sinalFiltrado = FIRFilter(sinalNormalizado,48000,60.0,4000.0)
 	AM=generateAm(12000,sinalFiltrado,48000,int(tecla[1]))
+
+	sinal.plotFFT(myrecording,48000)
+	plt.show()
+	sinal.plotFFT(sinalNormalizado,48000)
+	plt.show()
+	sinal.plotFFT(sinalFiltrado,48000)
+	plt.show()
+	sinal.plotFFT(AM,48000)
+	plt.show()
 
 	plotGraphs(myrecording,sinalNormalizado)
 	plotGraphs(sinalFiltrado,AM)
@@ -55,18 +77,19 @@ if tecla[0] == "s":
 	sd.wait()
 
 elif tecla=="fs":
-	myrecording, fs = sf.read("senda.ogg", dtype='float32')
-	fs=48000
+	data, fs = sf.read("senda.ogg", dtype='float32')
+	#print(len(data))
+	#fs=48000
+	data /= np.max(data) #normalizando o vetor do audio do arquivo
+	time = len(data)/fs
+	AM=generateAm(12000,data,fs,time)
+	AMClean= generateAm(12000,AM,fs,time)
+	final = FIRFilter(AMClean,fs,60,12000)
+	#print(np.max(AM))
 
-	sinalNormalizado = myrecording/np.max(myrecording) #normalizando o vetor do audio do arquivo
-	time = len(sinalNormalizado)/fs
-	sinalFiltrado = FIRFilter(sinalNormalizado,fs,60.0,4000.0)
-	AM=generateAm(12000,sinalFiltrado,fs,time)
+	tryPlot(AM,fs,time)
 
-	plotGraphs(myrecording,sinalNormalizado)
-	plotGraphs(sinalFiltrado,AM)
 
-	print(AM)
 
 	sd.play(AM,fs)
 	sd.wait()
@@ -74,7 +97,7 @@ elif tecla=="fs":
 elif tecla[0] == "r":
 
 	fs=48000
-	myrecording = sd.rec(int(10*fs), samplerate=fs, channels=1) #TODO: fazer uma funcao para esta linha 
+	myrecording = sd.rec(int(int(tecla[1])*fs), samplerate=fs, channels=1) #TODO: fazer uma funcao para esta linha
 	sd.wait()
 	myrecording = np.ndarray.flatten(myrecording)
 	time = len(myrecording)/fs
